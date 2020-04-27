233ce6cc6030ddc0ff1d90a267c243f42e7d9508
Dan <shakeylfc@gmail.com>
2020-04-27 23:40:33 +0100

    some finally changes for using react context api

--- my-app/src/context/auth-context.js
+++ my-app/src/context/auth-context.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const authContext = React.createContext({});
+const authContext = React.createContext({
+    authenticated : false,
+    login: () => {
+        
+    }
+});
 
 export default authContext;
