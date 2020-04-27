920ce3081d8facc99fa04bbc1712923e80872bfb
Dan <shakeylfc@gmail.com>
2020-04-28 00:49:23 +0100

    Added layout component. Start of burger builder functionality. Added aux hoc also for JSX rendering.

---
+++ burger-builder/src/components/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Auxiliary from '../../../hoc/Auxiliary';
+import Burger from '../../Burger/Burger';
+
+class BurgerBuilder extends Component {
+
+    render () {
+        return (
+            <Auxiliary>
+                <Burger />
+                <div>Build Controls</div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default BurgerBuilder;
